a85adc2b47f5554624819534241b5761d1293a74
flashniv <flashniv@gmail.com>
2022-07-02 11:46:08 +0300

    init

---
+++ src/main/java/ua/com/serverhelp/simplemetricstoragefile/SimpleMetricStorageFileApplication.java
@@ -0,0 +1,13 @@
+package ua.com.serverhelp.simplemetricstoragefile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMetricStorageFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMetricStorageFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/com/serverhelp/simplemetricstoragefile/SimpleMetricStorageFileApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.serverhelp.simplemetricstoragefile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMetricStorageFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
