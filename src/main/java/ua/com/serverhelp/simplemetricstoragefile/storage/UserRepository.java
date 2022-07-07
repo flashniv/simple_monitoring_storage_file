1c13c4d9d075a538051870853f58bce7d003e8bd
flashniv <flashniv@gmail.com>
2022-07-07 19:18:28 +0300

    rest adding. Add Cacheable Part 6.0

--- src/main/java/ua/com/serverhelp/simplemetricstoragefile/storage/UserRepository.java
+++ src/main/java/ua/com/serverhelp/simplemetricstoragefile/storage/UserRepository.java
@@ -1,8 +1,10 @@
 package ua.com.serverhelp.simplemetricstoragefile.storage;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.com.serverhelp.simplemetricstoragefile.entities.account.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    @Cacheable(value = "users")
     User findByUsername(String username);
 }
--- src/test/java/ua/com/serverhelp/simplemetricstoragefile/SimpleMetricStorageFileApplicationTests.java
+++ src/test/java/ua/com/serverhelp/simplemetricstoragefile/SimpleMetricStorageFileApplicationTests.java
@@ -2,8 +2,10 @@ package ua.com.serverhelp.simplemetricstoragefile;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootTest
+@EnableCaching
 class SimpleMetricStorageFileApplicationTests {
 
 	@Test
